b0aeff7ab42e257acf79d811b4fd27544a929a1f
Flakeparadigm <flakeparadigm@gmail.com>
2020-10-17 21:58:56 +1100

    [ADD] ability to log from specific commit

--- src/commands/log.ts
+++ src/commands/log.ts
@@ -1,17 +1,27 @@
-import { CommandModule } from 'yargs';
+import { Arguments, Argv, CommandModule } from 'yargs';
 import { getCommit } from '../base';
 import { getHead } from '../data';
 
-export default class LogCommand implements CommandModule {
-    public command = 'log';
+type LogArgs = {
+    object: string,
+};
+
+export default class LogCommand implements CommandModule<unknown, LogArgs> {
+    public command = 'log [object]';
 
     public description = 'show commit logs';
 
-    public builder = {};
+    public builder(yargs: Argv): Argv<LogArgs> {
+        return yargs
+            .positional('object', {
+                description: 'hash of object to start the log from',
+                type: 'string',
+            }) as Argv<LogArgs>;
+    }
 
-    public handler(): void {
+    public handler(args: Arguments<LogArgs>): void {
         const repoPath = process.cwd();
-        let objectId = getHead(repoPath);
+        let objectId = args.object || getHead(repoPath);
 
         while (objectId) {
             const commit = getCommit(repoPath, objectId);
